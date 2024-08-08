dfbe14f3bb5eaa627fd9f9bf35856115a5005eb9
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-08-08 00:20:38 -0300

    Spanner and collision

--- Assets/Scripts/Manager/SpannerManager.cs
+++ Assets/Scripts/Manager/SpannerManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpannerManager : MonoBehaviour
 {
+    // referencia ao inimigo
+    public GameObject enemy;
+
+    // lista para receber os pontos de spawn
+    public Transform[] spawnPoints;
+
+    [Header("Spawner Attributes")]
+    public float timeBetweenSpawns;
+    public float nextSpawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > nextSpawnTime)
+        {
+            // Seleciona um ponto de spawn aleatório e pega a posição
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+
+            // Atualiza o próximo tempo de spawn
+            nextSpawnTime = Time.time + timeBetweenSpawns;
+        }
     }
 }
